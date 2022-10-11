721eeaeb212bebbe14cbba731a0421731642f404
Nirav_Prajapati <niravprajapati97120@gmail.com>
2022-10-11 14:09:44 +0530

    Update firebase.js

--- todo-app/src/firebase.js
+++ todo-app/src/firebase.js
@@ -3,13 +3,6 @@ import 'firebase/compat/firestore';
 
 const firebaseApp = firebase.initializeApp({
 
-    apiKey: "AIzaSyCQnd4JlT00tVgBalaeM2FmxHYSMvlYLO8",
-    authDomain: "todo-89420.firebaseapp.com",
-    projectId: "todo-89420",
-    storageBucket: "todo-89420.appspot.com",
-    messagingSenderId: "596898004646",
-    appId: "1:596898004646:web:fee5f4b0e4f1d34549be67",
-    measurementId: "G-PMPQ0HBJZT"
   
 });
 
