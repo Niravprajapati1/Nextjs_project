7755522fcbd532286c1160ad10c1e23a35adab4a
Nirav_Prajapati <niravprajapati97120@gmail.com>
2022-10-12 09:50:45 +0000

    deployed

--- todo-app/src/firebase.js
+++ todo-app/src/firebase.js
@@ -2,13 +2,15 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyCQnd4JlT00tVgBalaeM2FmxHYSMvlYLO8",
-    authDomain: "todo-89420.firebaseapp.com",
-    projectId: "todo-89420",
-    storageBucket: "todo-89420.appspot.com",
-    messagingSenderId: "596898004646",
-    appId: "1:596898004646:web:c91169e611293dd349be67",
-    measurementId: "G-57BC22PY34"
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    apiKey: "AIzaSyAjWc7JDBeT_5iT7FjDpydDtlLmvL-o430",
+    authDomain: "my-todo-9.firebaseapp.com",
+    projectId: "my-todo-9",
+    storageBucket: "my-todo-9.appspot.com",
+    messagingSenderId: "640273002067",
+    appId: "1:640273002067:web:459f131f421a6810cbe0a6",
+    measurementId: "G-PWP39Q19WZ"
+  
 });
 
 const db = firebaseApp.firestore();
